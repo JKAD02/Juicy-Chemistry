c08657f3879239a5f2489705852529f8627123b9
Durgesh Kumar Singh <durgeshkumarsingh8787@gmail.com>
2021-11-13 17:01:25 +0530

    improved shipping page

--- shipping.js
+++ shipping.js
@@ -9,9 +9,9 @@ function GoToPaymentPage() {
     var f = document.getElementById("countryn").value;
     var g = document.getElementById("staten").value;
     var h = document.getElementById("pinc").value;
-    if(a.length === 0 || b.length === 0 || c.length === 0 || d.length === 0 || e.length === 0 || f.length === 0 || g.length === 0 || h.length === 0) {
+    var i =  document.getElementById("phnen").value;
+    if(a.length === 0 || b.length === 0 || c.length === 0 || d.length === 0 || e.length === 0 || f.length === 0 || g.length === 0 || h.length === 0 || i.length === 0) {
         alert("Please fill up all input fields");
-        console.log("here");
     } else {
         window.location = "payment.html";
     }
