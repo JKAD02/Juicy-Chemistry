0963a8997f98fbe3ed6765710bd9c21b970e2973
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2021-11-13 15:37:37 +0530

    otp modification

--- otp.js
+++ otp.js
@@ -2,7 +2,7 @@ document.getElementById("fPayment").addEventListener("click", finalPayment);
 
 function finalPayment() {
     var x = document.getElementById("otp").value;
-    if (x == 123456) {
+    if (x.length == 6) {
         window.location = "success.html"
     } else {
         alert("Invalid OTP")
